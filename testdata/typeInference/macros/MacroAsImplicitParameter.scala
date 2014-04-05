dea28d524287b8cf0e2842433c480dd5601df2c1
Konstantin Fedorov <k.a.feodorov@gmail.com>
2014-04-05 13:50:06 +0400

    macros mock in MethodInvocation

---
+++ testdata/typeInference/macros/MacroAsImplicitParameter.scala
@@ -0,0 +1,18 @@
+//scala/test/files/run/macro-expand-implicit-macro-is-val/Impls_1.scala
+import language.experimental.macros
+import scala.reflect.macros.whitebox.Context
+
+object MacroAsImplicitParameter {
+  def foo(c: Context): c.Expr[AnyVal] = {
+    import c.universe._
+    val body = Literal(Constant(2))
+    c.Expr[Int](body)
+  }
+}
+
+object Test extends App {
+  implicit def foo: AnyVal = macro MacroAsImplicitParameter.foo
+  def bar(implicit x: AnyVal) = x
+  /*start*/bar/*end*/
+}
+//Int
